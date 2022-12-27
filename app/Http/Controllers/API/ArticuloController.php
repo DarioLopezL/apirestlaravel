2d592eba5b80379135d97e0310fe374e79146f61
DarioLopezL <dariiolopez17@gmail.com>
2022-12-26 21:13:37 -0600

    first commit

---
+++ app/Http/Controllers/API/ArticuloController.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\Articulo;
+use Illuminate\Http\Request;
+
+class ArticuloController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Articulo::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $articulo = new Articulo();
+        $articulo->descripcion = $request->descripcion;
+        $articulo->precio = $request->precio;
+        $articulo->stock=$request->stock;
+
+        $articulo->save();
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request)
+    {
+        $articulo =  Articulo::findOrFail($request->id);
+        $articulo->descripcion = $request->descripcion;
+        $articulo->precio = $request->precio;
+        $articulo->stock=$request->stock;
+
+        $articulo->save();
+        return $articulo;
+
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Request $request)
+    {
+        $articulo = $articulo=Articulo::destroy($request->id);
+        return $articulo;
+    }
+}
